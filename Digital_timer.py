d04d194ad2e5d436e2224150c0ac207aa64f756c
Chill-coder69 <sungjinwoo2084@gmail.com>
2025-03-31 18:54:09 +0530

    Digital_timer.py

--- Digital_timer.py
+++ Digital_timer.py
@@ -1,25 +1,28 @@
 import time
-a=input("timer duration : ")
-b=int(a.split(" ")[0])
-if "sec" in a :
-    for y in range(b,0,-1):
+a=input("timer duration : ")           # take user input for time
+b=int(a.split(" ")[0])                 # seperate out the no. from input
+if "sec" in a :                        # run for seconds
+    for y in range(b,0,-1):            # run in reverse
         sec = int(y%60)
         min = int(y/60)
         hr = int(y/3600)
-        print(f"{hr:02}:{min:02}:{sec:02}" , end="\r")
+        print(f"{hr:02}:{min:02}:{sec:02}" , end="\r") # print the timer and rewrite the previous print
         time.sleep(1)
-if "min" in a :
-    for y in range(b*60,0,-1):
+elif "min" in a :                       # run for minutes
+    for y in range(b*60,0,-1):        # run in reverse
         sec = int(y%60)
         min = int(y/60)
         hr = int(y/3600)
-        print(f"{hr:02}:{min:02}:{sec:02}" , end="\r")
+        print(f"{hr:02}:{min:02}:{sec:02}" , end="\r")  # print the timer and rewrite the previous print
         time.sleep(1)
-if "hr" in a :
-    for y in range(b*3600,0,-1):
+elif "hr" in a :                       # run for hours
+    for y in range(b*3600,0,-1):     # run in reverse
         sec = int(y%60)
         min = int(y/60)
         hr = int(y/3600)
-        print(f"{hr:02}:{min:02}:{sec:02}" , end="\r")
+        print(f"{hr:02}:{min:02}:{sec:02}" , end="\r") # print the timer and rewrite the previous print
         time.sleep(1)
+else :
+    print("Invalid input")
+    exit()
 print("TIME'S UP")
