1a8d00c528f2f2d56cdcc83a573656a4f3dc7089
Chill-coder69 <sungjinwoo2084@gmail.com>
2025-04-01 11:58:11 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,20 @@
-operation=input("enter your operator : ")                      # choose the opeation(*,/,+,-)
-if operation not in ["+","-","*","/"]:                        # detects any invalid input
+# choose the opeation(*,/,+,-)
+operation=input("enter your operator : ")
+
+# detects any invalid input
+if operation not in ["+","-","*","/"]:
     print("your operator in not valid")    
-    exit()                                                       # exit on invalid input
-first_no=int(input("enter first no. : "))                        # first number
-sec_no=int(input("enter second no. : "))                         # second number
-result= {"/": c/b ,'*':c*b , "+":c+b , "-" : c-b}                # results
-print(f"{first_no} {operation} {sec_no} is {result[operation]}") # give output
+    # exit on invalid input
+    exit()
+
+# first number
+first_no=int(input("enter first no. : "))
+
+# second number
+sec_no=int(input("enter second no. : "))
+
+# results
+result= {"/": c/b ,'*':c*b , "+":c+b , "-" : c-b}
+
+# give output
+print(f"{first_no} {operation} {sec_no} is {result[operation]}")
