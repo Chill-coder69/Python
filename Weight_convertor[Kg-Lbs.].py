d44d3692991869112e813cb9f2384a07e2d02168
Chill-coder69 <sungjinwoo2084@gmail.com>
2025-04-01 12:09:12 +0530

    Update Weight_convertor[Kg-Lbs.].py

--- Weight_convertor[Kg-Lbs.].py
+++ Weight_convertor[Kg-Lbs.].py
@@ -1,9 +1,17 @@
-Your_conversion=input("you want to convert pound to kg or kg to pound : ") # enter your weight here
-if Your_conversion=="pound to kg":                                         # check if convertion in pound to kg
+# enter your weight here
+Your_conversion=input("you want to convert pound to kg or kg to pound : ")
+
+# check if convertion in pound to kg
+if Your_conversion=="pound to kg":
     your_weight_in_pound=int(input("enter weight in pound : "))
-    print(f"the kg is :{your_weight_in_pound*0.453592} ")                  # give output in kg
-elif Your_conversion=="kg to pound":                                       # check if convertion in kg to pound
+# give output in kg
+    print(f"the kg is :{your_weight_in_pound*0.453592} ")
+    
+# check if convertion in kg to pound
+elif Your_conversion=="kg to pound":
     your_weight_in_kg=int(input("enter weight in kg : "))
-    print(f"the pound is :{your_weight_in_kg*2.20462}")                    # give output in pounds
+    # give output in pounds
+    print(f"the pound is :{your_weight_in_kg*2.20462}")
+# detects any invalid output
 else:
-    print(f"")
+    print("Invalid input")
