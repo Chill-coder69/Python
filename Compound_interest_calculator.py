261663a96deb7280be1c9fdae3d4be19c93ce33a
Chill-coder69 <sungjinwoo2084@gmail.com>
2025-04-06 22:05:31 +0530

    Compound_interest_calculator.py

--- Compound_interest_calculator.py
+++ Compound_interest_calculator.py
@@ -1,17 +1,17 @@
 # user input principal amount
-p =int(input("enter your principal value : "))
+p = int(input("Enter your principal value: "))
 
-# user input the rate
-r =int(input("enter your rate : "))
+# user input the rate (as a percentage, needs to be converted to decimal)
+r = float(input("Enter your rate (annual interest rate in %): ")) / 100
 
-# user input no. of time the interest is compounded
-n =int(input("enter number of times that interest is compounded per year : "))
+# user input number of times the interest is compounded per year
+n = int(input("Enter number of times that interest is compounded per year: "))
 
 # user input time
-t = int(input("enter the number of years the money is invested or borrowed for : "))
+t = int(input("Enter the number of years the money is invested or borrowed for: "))
 
-# put each variable in formula and calculate
-a = p*(1 + r/n)(n*t)
+# calculate compound interest
+a = p * (1 + r / n) ** (n * t)
 
 # print the final amount
-print(f"your final amount is {a}")
+print(f"Your final amount is {a:.2f}")
